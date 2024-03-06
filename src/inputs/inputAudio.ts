a6562f7cb88a2de539e9ce5e945e033eaa04fff9
ichbinrob <robinson.lacotte@pm.me>
2024-03-06 15:19:53 +0100

    clean inputAudio

--- src/inputs/inputAudio.ts
+++ src/inputs/inputAudio.ts
@@ -1,56 +1,56 @@
 import { Analyser, Scene, Engine, Sound, SoundTrack } from "@babylonjs/core";
 declare global {
   interface Window {
-    stream: any;
-    constraints: any;
+    stream: MediaStream;
+    constraints: MediaStreamConstraints;
   }
 }
 export class InputAudio {
-  private scene: Scene;
-  private constraints: any;
-  private analyser: Analyser;
-  private audioReady: boolean;
-  private debug: Debug;
-  private kicks: [Kick];
+  private _scene: Scene;
+  private _constraints: MediaStreamConstraints;
+  private _analyser: Analyser;
+  private _audioReady: boolean;
+  private _debug: Debug;
+
   constructor(scene: Scene) {
-    this.audioReady = false;
-    this.scene = scene;
-    this.constraints = window.constraints = {
+    this._audioReady = false;
+    this._scene = scene;
+    this._constraints = window.constraints = {
       audio: true,
       video: false,
     };
     this.handleSuccess = this.handleSuccess.bind(this);
 
     navigator.mediaDevices
-      .getUserMedia(this.constraints)
+      .getUserMedia(this._constraints)
       .then(this.handleSuccess)
       .catch(this.handleError);
   }
 
-  handleSuccess(stream) {
+  handleSuccess(stream: MediaStream) {
     const audioTracks = stream.getAudioTracks();
     console.log("Got stream with constraints:", window.constraints);
     console.log("Using audio device: " + audioTracks[0].label);
-    stream.oninactive = function () {
+    stream.onremovetrack = function () {
       console.log("Stream ended");
     };
     window.stream = stream; // make variable available to browser console
 
-    var bjsSound = new Sound("mic", stream, this.scene);
+    var bjsSound = new Sound("mic", stream, this._scene);
     //bjsSound.attachToMesh(sphere);
     bjsSound.play();
-    var soundTrack = new SoundTrack(this.scene);
+    var soundTrack = new SoundTrack(this._scene);
     soundTrack.addSound(bjsSound);
-    this.analyser = new Analyser(this.scene);
+    this._analyser = new Analyser(this._scene);
 
-    soundTrack.connectToAnalyser(this.analyser);
-    this.analyser.FFT_SIZE = 2048;
-    this.analyser.SMOOTHING = 0.9;
+    soundTrack.connectToAnalyser(this._analyser);
+    this._analyser.FFT_SIZE = 2048;
+    this._analyser.SMOOTHING = 0.9;
 
-    this.debug = new Debug(this.analyser);
+    this._debug = new Debug(this._analyser);
 
-    console.log(this.analyser);
-    this.audioReady = true;
+    console.log(this._analyser);
+    this._audioReady = true;
     // Low Gain 0-500 Hz
     // Mid Gain 500-4500Hz
     // High Gain 4500 +
@@ -61,200 +61,189 @@ export class InputAudio {
   }
 
   registerBeforeRender() {
-    // var workingArray = myAnalyser.getByteFrequencyData();
-
-    // for (var i = 0; i < myAnalyser.getFrequencyBinCount() ; i++) {
-    //     spatialBoxArray[i].scaling.y =  workingArray[i] / 32;
-    // }
-    //creates array, copies frequency data into the passed unsigned byte array
-    //var workingArrayFreq = myAnalyser.getByteFrequencyData();
-
-    if (this.audioReady) {
+    if (this._audioReady) {
       // UNCOMMENT TO SHOW DEBUG
-       this.debug.draw();
+       this._debug.draw();
 
-      var workingArrayTime = this.analyser.getByteFrequencyData();
-
+      var workingArrayTime = this._analyser.getByteFrequencyData();
       //loop scales mesh with each value
-      for (var i = 0; i < this.analyser.getFrequencyBinCount(); i++) {
+      for (var i = 0; i < this._analyser.getFrequencyBinCount(); i++) {
         //frequency bin count is 1/2 of FFT_SIZE
         var size = workingArrayTime[i];
-        //console.log("i : ", i,  " -> ", size)
-        // speaker1.scaling = new BABYLON.Vector3(size,size,size);
-        // speaker2.scaling = new BABYLON.Vector3(size,size,size);
       }
     }
   }
 }
 
 class Debug {
-  private analyser: Analyser;
-  private canvas: HTMLCanvasElement;
-  private ctx: CanvasRenderingContext2D;
+  private _analyser: Analyser;
+  private _canvas: HTMLCanvasElement;
+  private _ctx: CanvasRenderingContext2D;
+  private _borderHeight:number;
+  private _spectrumLength:number;
+  private _spectrumWidth:number;
+  private _spectrumHeight:number;
+  private _spectrum:Uint8Array;
+  
   constructor(analyser: Analyser) {
-    this.analyser = analyser;
+    this._analyser = analyser;
 
-    this.canvas = document.createElement("canvas");
-    this.canvas.width = 512;
-    this.canvas.height = 300;
-    this.canvas.style.position = "absolute";
-    this.canvas.style.bottom = "0";
-    this.canvas.style.left = "0";
-    this.canvas.style.zIndex = "3";
-    document.body.appendChild(this.canvas);
-    this.ctx = this.canvas.getContext("2d")!;
+    this._canvas = document.createElement("canvas");
+    this._canvas.width = 512;
+    this._canvas.height = 300;
+    this._canvas.style.position = "absolute";
+    this._canvas.style.bottom = "0";
+    this._canvas.style.left = "0";
+    this._canvas.style.zIndex = "3";
+    document.body.appendChild(this._canvas);
+    this._ctx = this._canvas.getContext("2d")!;
+    this._borderHeight = 10;
 
     window.addEventListener("resize", this.resize.bind(this), false);
     this.resize();
   }
 
   resize() {
-    this.canvas.width = window.innerWidth;
+    this._canvas.width = window.innerWidth;
   }
 
   draw() {
 
-    let borderHeight = 10;
+    this._spectrum = this._analyser.getByteFrequencyData();
+    this._spectrumLength = this._analyser.getFrequencyBinCount();
+    this._spectrumWidth = this._canvas.width / this._spectrumLength;
+    this._spectrumHeight = this._canvas.height - this._borderHeight;
 
-    // draw background
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.beginPath();
-    this.ctx.rect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.fillStyle = "#000000";
-    this.ctx.fill();
-    this.ctx.strokeStyle = "#a1a1a1";
-    this.ctx.stroke();
+    this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+    //draw background
+    this.drawBackground();
 
     // draw spectrum
-    this.ctx.beginPath();
-    let spectrum = this.analyser.getByteFrequencyData();
-    let spectrumLength = this.analyser.getFrequencyBinCount();
-    let spectrumWidth = this.canvas.width / spectrumLength;
-    let spectrumHeight = this.canvas.height - borderHeight;
+    this.drawSpectrum();
+
+    // draw frequency
+    this.drawFrequency();
+    // draw kick
+    // this.drawKick();
+
+    // draw waveform
+    this.drawWaveForm();
+  }
+
+  drawBackground() {
+    this._ctx.beginPath();
+    this._ctx.rect(0, 0, this._canvas.width, this._canvas.height);
+    this._ctx.fillStyle = "#000000";
+    this._ctx.fill();
+    this._ctx.strokeStyle = "#a1a1a1";
+    this._ctx.stroke();
+  }
+
+  drawSpectrum() {
+    this._ctx.beginPath();
     const split1 = 300;
     const split2 = 630;
     const lowGain = 0.2;
     const midGain = 1;
     const highGain = 3;
-    for (let i = 0; i < spectrumLength; i++) {
-      let spectrumValue = spectrum[i] / 256;
-      this.ctx.rect(
-        i * spectrumWidth,
-        spectrumHeight - spectrumHeight * spectrumValue,
-        spectrumWidth / 2,
-        spectrumHeight * spectrumValue
+    for (let i = 0; i < this._spectrumLength; i++) {
+      let spectrumValue = this._spectrum[i] / 256;
+      this._ctx.rect(
+        i * this._spectrumWidth,
+        this._spectrumHeight - this._spectrumHeight * spectrumValue,
+        this._spectrumWidth / 2,
+        this._spectrumHeight * spectrumValue
       );
     }
-    this.ctx.fillStyle = "#ffffff";
-    this.ctx.fill();
+    this._ctx.fillStyle = "#ffffff";
+    this._ctx.fill();
 
-    this.ctx.beginPath();
-    this.ctx.globalAlpha = 0.3;
-    for (let i = 0; i < spectrumLength; i++) {
-      let spectrumValue = spectrum[i] / 256;
+    this._ctx.beginPath();
+    this._ctx.globalAlpha = 0.3;
+    for (let i = 0; i < this._spectrumLength; i++) {
+      let spectrumValue = this._spectrum[i] / 256;
       let sectionWidth = split1;
       let sectionStart = 0;
       if (i <= split1) {
-        sectionWidth = spectrumWidth * split1;
+        sectionWidth = this._spectrumWidth * split1;
         sectionStart = 0;
         spectrumValue *= lowGain;
       } else if (i > split1 && i < split2) {
-        sectionWidth = spectrumWidth * (split2 - split1);
-        sectionStart = spectrumWidth * split1;
+        sectionWidth = this._spectrumWidth * (split2 - split1);
+        sectionStart = this._spectrumWidth * split1;
         spectrumValue *= midGain;
       } else if (i >= split2) {
         // >= split2
-        sectionWidth = this.canvas.width - split2 * spectrumWidth;
-        sectionStart = spectrumWidth * split2;
+        sectionWidth = this._canvas.width - split2 * this._spectrumWidth;
+        sectionStart = this._spectrumWidth * split2;
         spectrumValue *= highGain;
       }
-      this.ctx.rect(
+      this._ctx.rect(
         sectionStart,
-        spectrumHeight - spectrumHeight * spectrumValue,
+        this._spectrumHeight - this._spectrumHeight * spectrumValue,
         sectionWidth,
-        spectrumHeight * spectrumValue
+        this._spectrumHeight * spectrumValue
       );
     }
-    this.ctx.fillStyle = "#e4e";
-    this.ctx.fill();
-    this.ctx.globalAlpha = 1;
+    this._ctx.fillStyle = "#e4e";
+    this._ctx.fill();
+    this._ctx.globalAlpha = 1;
+  }
 
-    // draw frequency
-    this.ctx.beginPath();
-    this.ctx.font = "10px Arial";
-    this.ctx.textBaseline = "middle";
-    this.ctx.textAlign = "left";
-    for (let i = 0, len = spectrumLength; i < len; i++) {
+  drawFrequency() {
+    this._ctx.beginPath();
+    this._ctx.font = "10px Arial";
+    this._ctx.textBaseline = "middle";
+    this._ctx.textAlign = "left";
+    for (let i = 0, len = this._spectrumLength; i < len; i++) {
       if (i % 10 == 0) {
-        this.ctx.rect(i * spectrumWidth, spectrumHeight, spectrumWidth / 2, borderHeight);
-        this.ctx.fillText(`${i}`, i * spectrumWidth + 4, spectrumHeight + borderHeight * 0.5);
+        this._ctx.rect(i * this._spectrumWidth, this._spectrumHeight, this._spectrumWidth / 2, this._borderHeight);
+        this._ctx.fillText(`${i}`, i * this._spectrumWidth + 4, this._spectrumHeight + this._borderHeight * 0.5);
       }
     }
-    this.ctx.fillStyle = "#e4e";
-    this.ctx.fill();
+    this._ctx.fillStyle = "#e4e";
+    this._ctx.fill();
+  }
 
-    // // draw kick
-    // let kicks = this.kicks;
-    // let kickLength = kicks.length;
-    // for (let i = 0, len = kickLength; i < len; i++) {
-
-    //     const kick = kicks[i];
-    //     if (kick.isOn) {
-    //         const kickFrequencyStart = (kick.frequency.length ? kick.frequency[0] : kick.frequency);
-    //         const kickFrequencyLength = (kick.frequency.length ? kick.frequency[1] - kick.frequency[0] + 1 : 1);
-    //         this.ctx.beginPath();
-    //         this.ctx.rect(kickFrequencyStart * spectrumWidth, spectrumHeight - spectrumHeight * (kick.threshold / 256), kickFrequencyLength * spectrumWidth - (spectrumWidth * .5), 2);
-    //         this.ctx.rect(kickFrequencyStart * spectrumWidth, spectrumHeight - spectrumHeight * (kick.currentThreshold / 256), kickFrequencyLength * spectrumWidth - (spectrumWidth * .5), 5);
-    //         this.ctx.fillStyle = kick.isKick ? '#00ff00' : '#ff0000';
-    //         this.ctx.fill();
-    //     }
-    // }
-
-    // draw waveform
-    this.ctx.beginPath();
-    let waveform = this.analyser.getByteTimeDomainData();
-    let waveformLength = this.analyser.getFrequencyBinCount();
-    let waveformWidth = this.canvas.width / waveformLength;
-    let waveformHeight = this.canvas.height - borderHeight;
+  drawWaveForm() {
+    this._ctx.beginPath();
+    let waveform = this._analyser.getByteTimeDomainData();
+    let waveformLength = this._analyser.getFrequencyBinCount();
+    let waveformWidth = this._canvas.width / waveformLength;
+    let waveformHeight = this._canvas.height - this._borderHeight;
     for (let i = 0; i < waveformLength; i++) {
       let waveformValue = waveform[i] / 256;
-      if (i == 0) this.ctx.moveTo(i * waveformWidth, waveformHeight * waveformValue);
-      else this.ctx.lineTo(i * waveformWidth, waveformHeight * waveformValue);
+      if (i == 0) this._ctx.moveTo(i * waveformWidth, waveformHeight * waveformValue);
+      else this._ctx.lineTo(i * waveformWidth, waveformHeight * waveformValue);
     }
-    this.ctx.strokeStyle = "#0000ff";
-    this.ctx.stroke();
-
-    // // draw time
-    // this.ctx.beginPath();
-    // this.ctx.textAlign = "right";
-    // this.ctx.textBaseline = 'top';
-    // this.ctx.font = "15px Arial";
-    // this.ctx.fillStyle = '#ffffff';
-    // this.ctx.fillText((Math.round(this.sound.time * 10) / 10) + ' / ' + (Math.round(this.sound.duration * 10) / 10), this.canvas.width - 5, 5);
-
-    // // draw section
-    // this.ctx.beginPath();
-    // let sections = this.sound._sections;
-    // let section = null;
-    // let sectionLength = sections.length;
-    // let sectionLabels = '';
-    // for (let i = 0, len = sectionLength; i < len; i++) {
-
-    //     section = sections[i];
-    //     if ( section.condition() ) {
-    //         sectionLabels += section.label + ' - ';
-    //     }
-    // }
-    // if (sectionLabels.length > 0) sectionLabels = sectionLabels.substr(0, sectionLabels.length - 3);
-    // this.ctx.fillText(sectionLabels, this.canvas.width - 5, 25);
-    // this.ctx.fill();
+    this._ctx.strokeStyle = "#0000ff";
+    this._ctx.stroke();
   }
+
+  // drawKick() {
+  //   let kicks = this.kicks;
+  //   let kickLength = kicks.length;
+  //   for (let i = 0, len = kickLength; i < len; i++) {
+
+  //       const kick = kicks[i];
+  //       if (kick.isOn) {
+  //           const kickFrequencyStart = (kick.frequency.length ? kick.frequency[0] : kick.frequency);
+  //           const kickFrequencyLength = (kick.frequency.length ? kick.frequency[1] - kick.frequency[0] + 1 : 1);
+  //           this._ctx.beginPath();
+  //           this._ctx.rect(kickFrequencyStart * this._spectrumWidth, this._spectrumHeight - this._spectrumHeight * (kick.threshold / 256), kickFrequencyLength * this._spectrumWidth - (this._spectrumWidth * .5), 2);
+  //           this._ctx.rect(kickFrequencyStart * this._spectrumWidth, this._spectrumHeight - this._spectrumHeight * (kick.currentThreshold / 256), kickFrequencyLength * this._spectrumWidth - (this._spectrumWidth * .5), 5);
+  //           this._ctx.fillStyle = kick.isKick ? '#00ff00' : '#ff0000';
+  //           this._ctx.fill();
+  //       }
+  //   }
+  // }
+
 }
 
 class Kick {
-  frequency: any;
-  threshold: any;
-  decay: any;
+  frequency: number[];
+  threshold: number;
+  decay: number;
   onKick: any;
   offKick: any;
   isOn: boolean;
